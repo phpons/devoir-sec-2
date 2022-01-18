8af3a098aa7c4eadc99993aa839ef552b3c085da
Pedro Pons <pedro-henrique.pons-fiorentin@grenoble-inp.org>
2022-01-18 15:43:05 +0100

    Updated docker-compose, mysql still broken

--- dockerfile
+++ dockerfile
@@ -1,5 +1,9 @@
-FROM php:7.2-apache
+FROM php:7.3-apache
+
 COPY erms /var/www/html/
+
 RUN docker-php-ext-install mysqli && docker-php-ext-enable mysqli
+RUN apt-get update && apt-get upgrade -y
+
 EXPOSE 80
 CMD ["/usr/sbin/apache2ctl", "-D", "FOREGROUND"]
--- erms/includes/dbconnection.php
+++ erms/includes/dbconnection.php
@@ -1,7 +1,14 @@
 <?php
-$con=mysqli_connect("Localhost", "root", "", "ermsdb");
+$db_host = "localhost"
+$db_port = "3306"
+$db_user = "root"
+$db_password = "supersecret"
+$db_name = "ermsdb"
+
+$con = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+
 if(mysqli_connect_errno()){
-echo "Connection Fail".mysqli_connect_error();
+  echo "Connection Fail".mysqli_connect_error();
 }
 
-  ?>
+?>
